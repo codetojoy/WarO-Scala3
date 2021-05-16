93d699ef3c79d2ebd793cff2c280894893dc8d78
Michael Easter <codetojoy@gmail.com>
2021-05-16 17:29:42 -0300

    incremental update Sun 16 May 2021 17:29:42 ADT

--- src/main/scala/org/peidevs/waro/domain/Player.scala
+++ src/main/scala/org/peidevs/waro/domain/Player.scala
@@ -5,37 +5,28 @@ import org.peidevs.waro.strategy._
 
 class Player(val name:String, val maxCard:Int, val strategy:Strategy,
                 var hand:List[Int], val playerStats:PlayerStats) {
-    
-    def this(name:String, maxCard:Int, strategy:Strategy) = {
+
+    def this(name:String, maxCard:Int, strategy:Strategy) =
         this(name, maxCard, strategy, List(), new PlayerStats(0,0,0))
-    }    
 
-    def this(name:String, maxCard:Int, strategy:Strategy, hand:List[Int]) = {
+    def this(name:String, maxCard:Int, strategy:Strategy, hand:List[Int]) =
         this(name, maxCard, strategy, hand, new PlayerStats(0,0,0))
-    }    
-    
-    def getBid(prizeCard:Int):Bid = {
 
+    def getBid(prizeCard:Int):Bid =
         val offer = strategy.selectCard(prizeCard, hand, maxCard)
-        hand = hand diff List(offer)        
+        hand = hand diff List(offer)
         new Bid(offer, this)
-    }
-    
-    def clear():Unit = {
+
+    def clear():Unit =
         hand = List()
         playerStats.clear()
-    }
-    
-    def winsRound(prizeCard:Int):Unit = {
+
+    def winsRound(prizeCard:Int):Unit =
         playerStats.winsRound(prizeCard)
-    }
-    
-    def winsGame():Unit = {
+
+    def winsGame():Unit =
         playerStats.winsGame()
-    }
-    
-    def emitLog():String = {
+
+    def emitLog():String =
         name + " " + playerStats.emitLog
-    }
-    
 }
