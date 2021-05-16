67f134afb797cf1ea440d573867054687b094a07
Michael Easter <codetojoy@gmail.com>
2021-05-16 17:30:55 -0300

    incremental update Sun 16 May 2021 17:30:55 ADT

--- src/main/scala/org/peidevs/waro/domain/PlayerStats.scala
+++ src/main/scala/org/peidevs/waro/domain/PlayerStats.scala
@@ -2,21 +2,17 @@
 package org.peidevs.waro.domain
 
 class PlayerStats(var total: Int, var numRoundsWon:Int, var numGamesWon:Int) {
-    def winsRound(prizeCard:Int) = {
+    def winsRound(prizeCard:Int): Unit =
         numRoundsWon = numRoundsWon + 1
-        total = total + prizeCard        
-    }
+        total = total + prizeCard
 
-    def winsGame() = {
+    def winsGame(): Unit =
         numGamesWon = numGamesWon + 1
-    }
-    
-    def clear() = {
+
+    def clear(): Unit =
         total = 0
         numRoundsWon = 0
-    }
-    
-    def emitLog:String = {
+
+    def emitLog:String =
         "(" + total + " , " + numRoundsWon + " , " + numGamesWon + " )"
-    }
 }
