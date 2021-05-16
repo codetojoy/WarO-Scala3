03a53d304cdcc5499535bd835138fdf6add09560
Michael Easter <codetojoy@gmail.com>
2021-05-16 17:26:32 -0300

    incremental update Sun 16 May 2021 17:26:32 ADT

--- src/main/scala/org/peidevs/waro/casino/Game.scala
+++ src/main/scala/org/peidevs/waro/casino/Game.scala
@@ -5,36 +5,33 @@ import org.peidevs.waro.domain._
 
 class Game(val deckFactory:DeckFactory) {
     val verbose = true
-    
-    def playGame(numCards:Int, players:List[Player]):Player = {
+
+    def playGame(numCards:Int, players:List[Player]):Player =
         val dealer = new Dealer(deckFactory)
         val table = dealer.deal(numCards, players)
         dealer.play(table)
         determineWinner(table)
-    }
-    
+
     // -- internal
-    
-    def determineWinner(table:Table):Player = {
+
+    def determineWinner(table:Table):Player =
         val kitty = table.kitty
         val players = table.players
 
         val winner = players.maxBy { _.playerStats.total }
         val max = winner.playerStats.total
 
-        if (verbose) {
+        if verbose then
             players.foreach { p =>
                 val numRounds = p.playerStats.numRoundsWon
                 val total = p.playerStats.total
                 println(p.name + " won " + numRounds + " rounds, with total : " + total)
-            }                            
-        }
+            }
 
         winner.winsGame()
         println("\nGame summary:")
         println("overall WINNER is: " + winner.name)
-        
-        return winner   
-    }
-    
+
+        return winner
+    end determineWinner
 }
