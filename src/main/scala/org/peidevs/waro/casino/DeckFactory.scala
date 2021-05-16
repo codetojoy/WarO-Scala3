04e7579338cec489e034381e32f78dfe5ef3857f
Michael Easter <codetojoy@gmail.com>
2021-05-16 17:25:03 -0300

    incremental update Sun 16 May 2021 17:25:02 ADT

--- src/main/scala/org/peidevs/waro/casino/DeckFactory.scala
+++ src/main/scala/org/peidevs/waro/casino/DeckFactory.scala
@@ -4,10 +4,7 @@ package org.peidevs.waro.casino
 import scala.util.Random
 
 class DeckFactory {
-        
-    def newDeck(numCards:Int):List[Int] = {
+    def newDeck(numCards:Int):List[Int] =
         var deck:List[Int] = (1 to numCards).toList
         Random.shuffle(deck)
-    }
-        
 }
