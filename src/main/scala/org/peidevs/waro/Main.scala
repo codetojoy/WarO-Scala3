27264fba9b71b3c536676f5ffc2f62c0be2e520c
Michael Easter <codetojoy@gmail.com>
2021-05-16 17:32:07 -0300

    incremental update Sun 16 May 2021 17:32:07 ADT

--- src/main/scala/org/peidevs/waro/Main.scala
+++ src/main/scala/org/peidevs/waro/Main.scala
@@ -4,11 +4,10 @@ package org.peidevs.waro
 import org.peidevs.waro.casino._
 
 object Main {
-    def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit =
         var config = new Config()
         var tourney = new Tourney(config)
         tourney.playGames()
-        
+
         println("done.")
-    }
 }
